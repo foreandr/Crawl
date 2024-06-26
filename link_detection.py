484e650cda81cdb277ecf850a3078dcfc1ae032d
foreandr <foreandr@gmail.com>
2024-06-26 16:11:56 -0400

    saddsdsa

--- link_detection.py
+++ link_detection.py
@@ -1,18 +1,18 @@
 import re
-
+# try agsani
 def identify_video_platform(url):
     patterns = {
         # YOUTUBE
         "channel": r'^https?://(?:www\.)?youtube\.com/(?:c(?:hannel)?/|@)?[A-Za-z0-9_-]+(?:/|$)',
         "content": r'^https?://(?:www\.)?youtube\.com/watch\?v=[A-Za-z0-9_-]+$',
-        
+
         # RUMBLE
         "channel": r'\/c\/[A-Za-z0-9]+$',
         "content": r'\/v[0-9a-z]{4,10}-[a-z0-9-.]+(?:\.html)?$'
     }
-    
+
     for platform, pattern in patterns.items():
         if re.search(pattern, url):
             return platform
-    
+
     return "Unknown"
